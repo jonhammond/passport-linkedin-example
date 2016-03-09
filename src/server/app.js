bbdbeb3a7832e6113b2de95e83d392b3d2d14eac
Wes Reid <bwreid@gmail.com>
2016-03-09 13:24:11 -0700

    Initialize passport.js in app.js
    * Include required packages and use LinkedInStrategy

--- src/server/app.js
+++ src/server/app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var cookieSession = require('cookie-session');
+var passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+
+if ( !process.env.NODE_ENV ) { require('dotenv').config(); }
 
 
 // *** routes *** //
@@ -31,7 +36,39 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: 'linkedin-oauth-session-example',
+  keys: [process.env.COOKIE_KEY1, process.env.COOKIE_KEY2]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, '../client')));
+
+// *** passport middleware *** //
+passport.use(new LinkedInStrategy({
+  clientID: process.env.LINKEDIN_API_KEY,
+  clientSecret: process.env.LINKEDIN_SECRET_KEY,
+  callbackURL: "http://localhost:3000/auth/linkedin/callback",
+  state: true
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function () {
+    return done(null, profile);
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  // later this will be where you selectively send to the browser 
+  // an identifier for your user, like their primary key from the 
+  // database, or their ID from linkedin
+
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // here is where you will go to the database and get the 
+  // user each time from it's id, after you set up your db
+  done(null, user)
+});
 
 
 // *** main routes *** //
