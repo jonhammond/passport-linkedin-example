066742f87f13ed8f691d49b2d53a833230bd4619
Wes Reid <bwreid@gmail.com>
2016-03-09 16:04:04 -0700

    Add HOST env var for heroku deploy

--- src/server/app.js
+++ src/server/app.js
@@ -50,7 +50,7 @@ app.use(passport.session());
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_API_KEY,
   clientSecret: process.env.LINKEDIN_SECRET_KEY,
-  callbackURL: "http://localhost:3000/auth/linkedin/callback",
+  callbackURL: process.env.HOST + "/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
   state: true
 }, function(accessToken, refreshToken, profile, done) {
